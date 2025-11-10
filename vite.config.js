9ff4a6e2335548b778f73dc3680c7d9830abca0a
Dean Collins <spacefrog29@gmail.com>
2025-11-10 11:16:41 +0000

    fix: set base path for production environment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/lruter/' : '/',
+  base: '/lruter/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
